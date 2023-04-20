7dd66e4d3116d539e13ae92d3a2624c3fe98250d
Yari <narducciyarigmail.com>
2023-04-20 17:48:32 +0200

    Aggiungere i file di progetto.

---
+++ adonet-db-videogame/VideogameManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace adonet_db_videogame
+{
+    public static class VideogameManager
+    {
+        private static string connessione = "Data Source=localhost;Initial Catalog=prova;Integrated Security=True";
+
+        public static void InserisciVideogame(Videogame videogame)
+        {
+            SqlConnection connesioneSql = new SqlConnection(connessione);
+
+            connesioneSql.Open();
+
+            string query = "INSERT INTO videogames VALUES(name,overview,release_date,created_at,updated_at,software_house_id)";
+
+            SqlCommand cmd = new SqlCommand(query, connesioneSql);
+            SqlParameter sqlParameter = cmd.Parameters.Add(new SqlParameter("name", "Valore"));
+            cmd.Parameters.Add(sqlParameter);
+
+            int affectedRows = cmd.EndExecuteNonQuery();
+
+            connesioneSql.Close();
+        }
+        
+    }
+}
